d8047d890b58e6747023bd9f8d1fa359a34db500
Jason <bilafish@gmail.com>
2021-04-10 23:49:01 +0800

    feat: add socket io

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,50 @@ const express = require("express");
 const socketio = require("socket.io");
 const cors = require("cors");
 
+const { addUser, removeUser, getUsersInRoom } = require("./users");
+
 const router = require("./router");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+  cors: true,
+  origins: ["https://localhost:3000"],
+});
 
-app.use(cors());
+// app.use(cors());
 app.use(router);
+
+io.on("connect", (socket) => {
+  console.log(`${socket.id} connected!`);
+
+  // Join room handler
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected.`);
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
 
 server.listen(process.env.PORT || 5000, () =>
   console.log(`Server has started.`)
