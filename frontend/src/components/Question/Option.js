3cb1fe8995700bf7e9103214e7d784598443d3cc
Jason <bilafish@gmail.com>
2021-04-13 18:45:38 +0800

    feat: add mcq option component

---
+++ frontend/src/components/Question/Option.js
@@ -0,0 +1,28 @@
+import { Box } from "@chakra-ui/react";
+
+const optionColours = ["#5582ac", "#f8f1ba", "#ea977d", "#e2c14d"];
+
+// Parse HTML entities in string
+export const parseEntities = (content) =>
+  new DOMParser().parseFromString(content, "text/html").body.innerText;
+
+const Option = ({ index, content, isSelected, onClickHandler }) => {
+  return (
+    <Box
+      p="1rem"
+      w="25%"
+      h="10rem"
+      borderRadius="lg"
+      borderWidth={isSelected ? "0.4rem" : "0px"}
+      borderColor="white"
+      cursor="pointer"
+      color="#151515"
+      bg={optionColours[index]}
+      onClick={onClickHandler}
+    >
+      {parseEntities(content)}
+    </Box>
+  );
+};
+
+export default Option;
