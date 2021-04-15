baae1ead82240d3645a879be5fb6547fe4df6322
Jason <bilafish@gmail.com>
2021-04-15 15:20:57 +0800

    tweak: show user's answer & correct answer for prev question round

--- frontend/src/components/GameView/index.js
+++ frontend/src/components/GameView/index.js
@@ -4,7 +4,40 @@ import QuestionTransition from "../Question/QuestionTransition";
 import { parseEntities } from "../Question/Option";
 import Timer from "../Question/Timer";
 import LeaderboardTable from "./LeaderboardTable";
+import { useSelector } from "react-redux";
+
+const getCurrentUserAnswer = (
+  currentUserSocketID,
+  playersList,
+  currentQnID,
+  currentQnNo,
+  questionsList
+) => {
+  const currentPlayer = playersList.find(
+    (player) => player.id === currentUserSocketID
+  );
+  if (currentPlayer === undefined) {
+    return "";
+  } else {
+    const playerAnswer = currentPlayer.answers[currentQnID];
+    if (playerAnswer === undefined) {
+      return "No answer submitted";
+    } else {
+      const playerAnsweredOption = questionsList[currentQnNo - 1].options.find(
+        (option) => option.id === playerAnswer.answerID
+      );
+      if (playerAnsweredOption === undefined) {
+        return "";
+      } else {
+        return playerAnsweredOption.payload;
+      }
+    }
+  }
+};
+
 const GameView = ({ selectedAnswer, selectOption, gameState, leaderboard }) => {
+  const { socketID } = useSelector((state) => state.user);
+
   const questionRoundStatus = gameState.questionRoundStatus;
   return (
     <>
@@ -43,7 +76,36 @@ const GameView = ({ selectedAnswer, selectOption, gameState, leaderboard }) => {
       )}
       {questionRoundStatus === "ended" && (
         <div>
-          <Heading>Leaderboard</Heading>
+          <Heading size="md">
+            Your Answer:{" "}
+            {gameState.currentQuestionNo <= gameState.questions.length &&
+              parseEntities(
+                getCurrentUserAnswer(
+                  socketID,
+                  gameState.players,
+                  gameState.questions[gameState.currentQuestionNo - 1].id,
+                  gameState.currentQuestionNo,
+                  gameState.questions
+                )
+              )}
+          </Heading>
+          <Heading size="md" mb="1rem">
+            Correct Answer:{" "}
+            {gameState.currentQuestionNo <= gameState.questions.length &&
+              parseEntities(
+                gameState.questions[
+                  gameState.currentQuestionNo - 1
+                ].options.find(
+                  (option) =>
+                    option.id ===
+                    gameState.questions[gameState.currentQuestionNo - 1]
+                      .answerID
+                ).payload
+              )}
+          </Heading>
+          <Heading size="sm" textAlign="center">
+            Leaderboard
+          </Heading>
           <LeaderboardTable data={leaderboard} />
         </div>
       )}
